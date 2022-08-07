b231267f1337e9d4adbcbecf75a3d98732668d60
Daniel Lisko <djlisko01@gmail.com>
2022-08-07 14:50:59 -0700

    Started homepage

---
+++ frontend/src/components/pages/HomeView.js
@@ -0,0 +1,22 @@
+const HomeView = () => {
+  return (
+    <div className="homepage">
+      <div className="bgColor">
+        <div className="row justify-content-center">
+          <div className="col-6" id="mainBox">
+            <h1 className="text-center fs-1 text fw-bolder">Daniel Lisko</h1>
+            <h2> Full Stack Web Developer </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quis
+              possimus eaque velit deserunt enim facere omnis repellat,
+              asperiores iste voluptas inventore recusandae aperiam rem
+              consequuntur expedita magnam sit in.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeView;
